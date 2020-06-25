6a2f7f1eeb8fa156852330efc8f58b5d28e50d9f
James Loucks <JLoucks@erisinfo.com>
2020-06-25 14:35:05 -0400

    tab check

--- US_Imagery/Historical_image_conversion_arc.py
+++ US_Imagery/Historical_image_conversion_arc.py
@@ -2,9 +2,12 @@
 import arcpy, os
 ##source files
 def image_properties(image_path):
-    sr = arcpy.SpatialReference(4326)
+    ## Check .TAB before converting to WGS84
+    tab_info = open(os.path.join(orig_image_file,image_path),'r')
+    print tab_info
+"""     sr = arcpy.SpatialReference(4326)
     arcpy.DefineProjection_management(os.path.join(orig_image_file,image_path),sr)
-    arcpy.Copy_management(os.path.join(orig_image_file,image_path), os.path.join(arc_image_output, image_path))
+    arcpy.Copy_management(os.path.join(orig_image_file,image_path), os.path.join(arc_image_output, image_path)) """
     return 'success'
 
 orig_image_file = r'C:\Users\JLoucks\Desktop\imagetest_US'
