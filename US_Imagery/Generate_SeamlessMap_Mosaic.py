ec707edeebb8ef26e3894b1397ef94c31b8e0e48
Hamid Kiavarz <HKiavarz@erisinfo.com>
2020-07-08 09:32:21 -0400

    add update seamless map function

--- US_Imagery/Generate_SeamlessMap_Mosaic.py
+++ US_Imagery/Generate_SeamlessMap_Mosaic.py
@@ -5,50 +5,77 @@
 # Created:     06/2020
 #-------------------------------------------------------------------------------
 import arcpy, os ,timeit      
-import cx_Oracle
 import json
 from arcpy.sa import *
-import pandas as pd
-import re
+
+
 from multiprocessing import Pool
 
 arcpy.CheckOutExtension("Spatial")
 
+def get_spatial_res(imagepath):
+
+    cellsizeX = arcpy.GetRasterProperties_management(imagepath,'CELLSIZEX')
+    cellsizeY = arcpy.GetRasterProperties_management(imagepath,'CELLSIZEY')
+    if cellsizeY > cellsizeX:
+        return str(cellsizeY)
+    else:
+        return str(cellsizeX)
+   
+def get_filesize(imagepath,ext):
+    imagepath = imagepath.replace('.TAB',ext)
+    return str(os.stat(imagepath).st_size)
+def get_imagepath(imagepath,ext):
+    imagepath = imagepath.replace('.TAB',ext)
+    return imagepath
 def get_year(filename,netpath):
-    for parse in re.split("\s|(?<!\d)[,.](?!\d)|\\\| |_|\.|\-",netpath):
-        if len(parse) == 4 and unicode(parse,'utf-8').isnumeric() and '19' in parse:
-            return parse
+    x = netpath.split('\\')
+    years = ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018','2019']
+    no_years = ['199x','200x','201x']
+    for item in x:
+        if item in years:
+            return item
             break
-        else:
-            for parse in filename.split('_'):
-                if len(parse) == 2 and unicode(parse,'utf-8').isnumeric():
-                    if parse in ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19']:
-                        return '20'+parse
-                        break
-                    else:
-                        return '19'+parse
-                        break
+    # if item is not in the years list
+    for item in x:
+        if item in no_years:
+             return item
+def get_Image_Metadata(imagepath):
+    dpi = 'NA'
+    bits = 'NA'
+    comp = 'NA'
+    width = 'NA'
+    length = 'NA'
+    # imagepath = filepath.replace('TAB',ext)
+    spatial_res = 'NA'
+    bits = arcpy.GetRasterProperties_management(imagepath,'VALUETYPE')
+    dpi = arcpy.GetRasterProperties_management(imagepath,'CELLSIZEX')
+    width = arcpy.GetRasterProperties_management(imagepath,'COLUMNCOUNT')
+    length = arcpy.GetRasterProperties_management(imagepath,'ROWCOUNT')
+    spatial_res = get_spatial_res(imagepath)
+    desc = arcpy.Describe(imagepath)
+    year = get_year(desc.baseName,desc.path)
+    
+    return [bits,width,length,imagepath,spatial_res,year]
 def get_Footprint(inputRaster):
     try:
         ws = arcpy.env.scratchFolder
         arcpy.env.workspace = ws
-        arcpy.env.overwriteOutput = True   
+        srWGS84 = arcpy.SpatialReference('WGS 1984')
         tmpGDB =os.path.join(ws,r"temp.gdb")
         if not os.path.exists(tmpGDB):
             arcpy.CreateFileGDB_management(ws,r"temp.gdb")
-        arcpy.AddMessage(inputRaster[0])
 
-        resampleRaster = os.path.join(ws,'resample' + inputRaster[1] + '.tif')
-        bin_Raster = os.path.join(ws,'bin_Raster' + inputRaster[1] + '.tif')
-        outputpolygon_with_holes= os.path.join(tmpGDB,'polygon_with_holes_'+ inputRaster[1])
+        # Calcuate Footprint geometry
+        resampleRaster = os.path.join(ws,'resampleRaster' + '.tif')
+        bin_Raster = os.path.join(ws,'bin_Raster' + '.tif')
+        polygon_with_holes= os.path.join(tmpGDB,'polygon_with_holes')
         out_Vertices = os.path.join(tmpGDB,'Out_Vertices')
-        
 
         arcpy.AddMessage('Start resampling the input raster...')
         start1 = timeit.default_timer()
-        print(resampleRaster)
-        rasterProp = arcpy.GetRasterProperties_management(inputRaster[0], "CELLSIZEX")
-        resample = arcpy.Resample_management(inputRaster[0],resampleRaster ,4, "NEAREST")
+        rasterProp = arcpy.GetRasterProperties_management(inputRaster, "CELLSIZEX")
+        resampleRaster = arcpy.Resample_management(inputRaster,resampleRaster ,4, "NEAREST")
         inputSR = arcpy.Describe(resampleRaster).spatialReference
         end1 = timeit.default_timer()
         arcpy.AddMessage(('End resampling the input raster. Duration:', round(end1 -start1,4)))
@@ -56,36 +83,35 @@ def get_Footprint(inputRaster):
  
         arcpy.AddMessage('Start creating binary raster (Raster Calculator)...')
         start2 = timeit.default_timer()
-        expression = 'Con(' + '"' + 'resample' + inputRaster[1] + '.tif' + '"' + ' >= 10 , 1)'
-        arcpy.AddMessage(bin_Raster)
+        expression = 'Con(' + '"' + 'resampleRaster' + '.tif' + '"' + ' >= 10 , 1)'
         bin_Raster = arcpy.gp.RasterCalculator_sa(expression, bin_Raster)
-       
         end2 = timeit.default_timer()
         arcpy.AddMessage(('End creating binary raster. Duration:', round(end2 -start2,4)))
 
-        ## Convert binary raster to polygon       
+        # Convert binary raster to polygon       
         arcpy.AddMessage('Start creating prime polygon from raster...')
-        start3 = timeit.default_timer()       
-        outputpolygon_with_holes = arcpy.RasterToPolygon_conversion(in_raster= bin_Raster, out_polygon_features=outputpolygon_with_holes, simplify="SIMPLIFY", raster_field="Value", create_multipart_features="SINGLE_OUTER_PART", max_vertices_per_feature="")
+        start3 = timeit.default_timer()  
+        # arcpy.RasterToPolygon_conversion(bin_Raster, 'primePolygon.shp', "SIMPLIFY", "VALUE")
+        polygon_with_holes =  arcpy.RasterToPolygon_conversion(in_raster= bin_Raster, out_polygon_features=polygon_with_holes, simplify="SIMPLIFY", raster_field="Value", create_multipart_features="SINGLE_OUTER_PART", max_vertices_per_feature="")
         end3 = timeit.default_timer()
         arcpy.AddMessage(('End creating polygon. Duration:', round(end3 -start3,4)))
 
-
+        ### extract the main polygon (with maximum area) which includes several donuts
         arcpy.AddMessage('Start extracting exterior ring (outer outline) of polygon...')
         start4 = timeit.default_timer()      
         sql_clause = (None, 'ORDER BY Shape_Area DESC')
         geom = arcpy.Geometry()
-        row = arcpy.da.SearchCursor(outputpolygon_with_holes,('SHAPE@'),None,None,False,sql_clause).next()
+        row = arcpy.da.SearchCursor(polygon_with_holes,('SHAPE@'),None,None,False,sql_clause).next()
         geom = row[0]
         end4 = timeit.default_timer()
         arcpy.AddMessage(('End extracting polygon. Duration:', round(end4 -start4,4)))
         
-        
-        arcpy.AddMessage('Start extracting exterior points and inserting as FC...')
+        ### extract the exterior points from main polygon to generate pure polygon from ouer line of main polygon
+        arcpy.AddMessage('Start extracting exterior points ...')
         start5 = timeit.default_timer()      
         outer_coords = []
         for island in geom.getPart():
-            arcpy.AddMessage("Vertices in island: {0}".format(island.count))
+            # arcpy.AddMessage("Vertices in island: {0}".format(island.count))
             for point in island:
                 # coords.append = (point.X,point.Y)
                 if not isinstance(point, type(None)):
@@ -98,35 +124,42 @@ def get_Footprint(inputRaster):
                         outer_coords.append((newPoint))
                         break
         
-        # # points_FC = arcpy.CreateFeatureclass_management(tmpGDB,"points_FC", "POINT", "", "DISABLED", "DISABLED", inputSR)
-        # # i = 0
-        # # with arcpy.da.InsertCursor(points_FC,["SHAPE@XY"]) as cursor: 
-        # #     for coord in outer_coords:
-        # #         cursor.insertRow([coord]) 
-        # #         i+= 1
-        # #         if i > 2:
-        # #             break       
-        # #     del cursor
+        # # # # points_FC = arcpy.CreateFeatureclass_management(tmpGDB,"points_FC", "POINT", "", "DISABLED", "DISABLED", inputSR)
+        # # # # i = 0
+        # # # # with arcpy.da.InsertCursor(points_FC,["SHAPE@XY"]) as cursor: 
+        # # # #     for coord in outer_coords:
+        # # # #         cursor.insertRow([coord]) 
+        # # # #         i+= 1
+        # # # #         if i > 2:
+        # # # #             break       
+        # # # #     del cursor
         
         ### Create footprint  featureclass -- > polygon 
-        footprint_FC = arcpy.CreateFeatureclass_management(tmpGDB,"footprint_FC_" + inputRaster[1], "POLYGON", "", "DISABLED", "DISABLED", inputSR)
+        footprint_FC = arcpy.CreateFeatureclass_management(tmpGDB,"footprint_FC", "POLYGON", "", "DISABLED", "DISABLED", inputSR)
         cursor = arcpy.da.InsertCursor(footprint_FC, ['SHAPE@'])
         cursor.insertRow([arcpy.Polygon(arcpy.Array([arcpy.Point(*coords) for coords in outer_coords]),inputSR)])
         del cursor
         end5 = timeit.default_timer()
         arcpy.AddMessage(('End extracting exterior points and inserted as FC. Duration:', round(end5 -start5,4)))
         
-        
-        
         arcpy.AddMessage('Start simplifying footprint polygon...')
         start6 = timeit.default_timer() 
         arcpy.Generalize_edit(footprint_FC, '100 Meter')
+        finalGeometry = (arcpy.da.SearchCursor(footprint_FC,('SHAPE@')).next())[0]
         end6 = timeit.default_timer()
         arcpy.AddMessage(('End simplifying footprint polygon. Duration:', round(end6 -start6,4)))
+        footprint_WGS84 = finalGeometry.projectAs(srWGS84)
+        return(footprint_WGS84)
     except:
         msgs = "ArcPy ERRORS:\n %s\n"%arcpy.GetMessages(2)
         arcpy.AddError(msgs)
         raise
+def UpdateSeamlessFC(DQQQ_footprint_FC,metaData,ft_Polygon):
+    srWGS84 = arcpy.SpatialReference('WGS 1984')
+    insertRow = arcpy.da.InsertCursor(DQQQ_footprint_FC, ['BITS','WIDTH','LENGTH','IMAGEPATH','SPATIAL_RESOLUTION','YEAR','SHAPE@'])
+    rowtuple=[str(metaData[0]),str(metaData[1]),str(metaData[2]),str(metaData[3]),str(metaData[4]),str(metaData[5]),ft_Polygon]
+    insertRow.insertRow(rowtuple)
+    del insertRow
 
 if __name__ == '__main__':
     
@@ -135,17 +168,36 @@ if __name__ == '__main__':
     arcpy.env.overwriteOutput = True   
     arcpy.AddMessage(ws)
     inputRaster = arcpy.GetParameterAsText(0)
-    footprint_FC = r'F:\Aerial_US\USImagery\Data\Seamless_Map.gdb\Aerial_Footprint_Mosaic'
+    DQQQ_footprint_FC = r'F:\Aerial_US\USImagery\Data\Seamless_Map.gdb\Aerial_Footprint_Mosaic'
+    DQQQ_ALL_FC = r'F:\Aerial_US\USImagery\Data\DOQQ_ALL.shp'
     
     startTotal = timeit.default_timer()
     
-    params = [[r'\\cabcvan1nas003\doqq\201x\MA\BARNSTABLE\2014\ortho_1-1_1n_s_ma001_2014_1.sid', 'a'],[r'\\cabcvan1nas003\doqq\201x\MA\FRANKLIN\2014\ortho_1-1_1n_s_ma011_2014_1.sid','b']]
-    pool = Pool(processes=2)
-    pool.map(get_Footprint, params)
-    # get_Footprint(r'\\cabcvan1nas003\doqq\201x\MA\BARNSTABLE\2014\ortho_1-1_1n_s_ma001_2014_1.sid')
-    # year = get_year('ortho_1-1_1n_s_ma003_2014_1','\\nas2520\doqq\201x\MA\BERKSHIRE\2014\ortho_1-1_1n_s_ma003_2014_1.TAB')
-    pool.close()
+    params =[]
+    # params = [[r'\\cabcvan1nas003\doqq\201x\MA\BARNSTABLE\2014\ortho_1-1_1n_s_ma001_2014_1.sid', 'a'],[r'\\cabcvan1nas003\doqq\201x\MA\FRANKLIN\2014\ortho_1-1_1n_s_ma011_2014_1.sid','b']]
+    # pool = Pool(processes=2)
+    # pool.map(get_Footprint, params)
     
+    # year = get_year('ortho_1-1_1n_s_ma003_2014_1','\\nas2520\doqq\201x\MA\BERKSHIRE\2014\ortho_1-1_1n_s_ma003_2014_1.TAB')
+    # pool.close()
+    
+    # get_Footprint(r'\\cabcvan1nas003\doqq\201x\MA\BARNSTABLE\2014\ortho_1-1_1n_s_ma001_2014_1.sid')
+    
+    with arcpy.da.SearchCursor(DQQQ_ALL_FC,["FID", 'TABLE_']) as dqq_cursor:
+         for row in dqq_cursor:
+            if row[0] > 4 :
+                 break   
+            print('FID = {}, Image = {}'.format(row[0], row[1])) 
+            rasterPath = row[1].replace('nas2520','cabcvan1nas003')
+            params.append([rasterPath.replace('TAB','sid'),str(row[0])])
+            footprint_Polygon = get_Footprint(rasterPath.replace('.TAB','.sid'))
+            metaData = get_Image_Metadata(rasterPath.replace('.TAB','.sid'))
+            UpdateSeamlessFC(DQQQ_footprint_FC,metaData,footprint_Polygon)
+
+    # pool = Pool(processes=2)
+    # pool.map(get_Footprint, params)       
+             
+        
     endTotal= timeit.default_timer()
     arcpy.AddMessage(('Total Duration:', round(endTotal -startTotal,4)))
     
