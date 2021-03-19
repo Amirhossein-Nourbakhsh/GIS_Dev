0d7e0fef1a688e9c6b57a9b3454e387055566518
awongerisgis <awong@erisinfo.com>
2021-03-19 14:45:13 -0400

    aw: reset to default

--- US_Topo/code/TOPO_US_config.py
+++ US_Topo/code/TOPO_US_config.py
@@ -22,36 +22,26 @@ def server_loc_config(configpath,environment):
     else:
         return 'invalid server configuration'
 
-def createScratch():
-    scratch = os.path.join(r"\\cabcvan1gis005\MISC_DataManagement\_AW\TOPO_US_SCRATCHY", "test123")
-    scratchgdb = "scratch.gdb"
-    if not os.path.exists(scratch):
-        os.mkdir(scratch)
-    if not os.path.exists(os.path.join(scratch, scratchgdb)):
-        arcpy.CreateFileGDB_management(scratch, "scratch.gdb")
-    return scratch, scratchgdb
-
-# # arcpy parameters
-# OrderIDText = arcpy.GetParameterAsText(0)
-# BufsizeText = arcpy.GetParameterAsText(1)
-# yesBoundary = arcpy.GetParameterAsText(2)
-# multipage = arcpy.GetParameterAsText(3)
-# gridsize = arcpy.GetParameterAsText(4)
-# scratch = arcpy.env.scratchWorkspace
-# scratchgdb = arcpy.env.scratchGDB
+# arcpy parameters
+OrderIDText = arcpy.GetParameterAsText(0)
+BufsizeText = arcpy.GetParameterAsText(1)
+yesBoundary = arcpy.GetParameterAsText(2)
+multipage = arcpy.GetParameterAsText(3)
+gridsize = arcpy.GetParameterAsText(4)
+scratch = arcpy.env.scratchWorkspace
+scratchgdb = arcpy.env.scratchGDB
 
 # order info
-order_obj = models.Order().get_order(21010800307)
+order_obj = models.Order().get_order(OrderIDText)
 
 # flags
-multipage = "N"                     # Y/N, for multipages, set yesBoundary to 'fixed' (not 'yes') if want boundary to display
-gridsize = "3 KiloMeters"           # for multipage grid
-yesBoundary = "yes"               # fixed/yes/no
-BufsizeText = "2.4"
-delyearFlag = "N"                   # Y/N, for internal use only, blank maps, etc.
+# multipage = "N"                     # Y/N, for multipages, set yesBoundary to 'fixed' (not 'yes') if want boundary to display
+# gridsize = "3 KiloMeters"           # for multipage grid
+# yesBoundary = "yes"               # fixed/yes/no
+# BufsizeText = "2.4"
+# delyearFlag = "N"                   # Y/N, for internal use only, blank maps, etc.
 
 # scratch file/folder outputs
-scratch, scratchgdb = createScratch()
 summaryPdf = os.path.join(scratch,'summary.pdf')
 coverPdf = os.path.join(scratch,"cover.pdf")
 shapePdf = os.path.join(scratch, 'shape.pdf')
