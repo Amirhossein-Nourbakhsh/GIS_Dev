3876badb686599037447b43c6c0aa74accb468fc
ERIS-jloucks <jloucks@erisinfo.com>
2021-04-19 14:28:25 -0400

    test data load service

--- DATA_LOAD/import_featureclass.py
+++ DATA_LOAD/import_featureclass.py
@@ -1,17 +1,28 @@
 import arcpy, os
-def getspatial_ref(infc):
+def getdb_connection(indbconnection):
+    us_load = str(r'\\cabcvan1gis005\GISData\US_LOAD_TEST.sde')
+    ca_load = str(r'\\cabcvan1gis005\GISData\CA_LOAD_TEST.sde')
+    if indbconnection == 'US_LOAD':
+        return us_load
+    elif indbconnection == 'CA_LOAD':
+        return ca_load
+    else:
+        arcpy.AddError('Could not connect to %s'%(indbconnection))
+
+def getspatial_ref(infc,outfc):
     spatial_ref = arcpy.Describe(infc).spatialReference
     if spatial_ref.factoryCode != 4326: #factory code for wgs84
-        arcpy.Project_management(infc,os.path.join(scratch,infc),4326)
-        infc = os.path.join(scratch,infc)
+        arcpy.Project_management(infc,os.path.join(scratch,outfc+'.shp'),4326)
+        infc = os.path.join(scratch,outfc+'.shp')
         return infc
     else:
         return infc
-indbconnection = r'\\cabcvan1gis005\GISData\Connection to GMTESTC.sde'#arcpy.GetParameterAsText(0) #location of connection file
-infc = r'C:\Users\JLoucks\Desktop\HFA_CO.shp'#arcpy.GetParameterAsText(1) This can be a shapefile or .gdb featureclass
-outfc = 'HFA_CO_test'#arcpy.GetParameterAsText(2) #name of table that will be imported to db
-scratch = r'C:\Users\JLoucks\Documents\JL\test1'
+indbconnection = str(arcpy.GetParameterAsText(0))#r'\\cabcvan1gis005\GISData\Connection to GMTESTC.sde'#arcpy.GetParameterAsText(0) #location of connection file
+infc = str(arcpy.GetParameterAsText(1))#r'C:\Users\JLoucks\Desktop\HFA_CO.shp'#arcpy.GetParameterAsText(1) This can be a shapefile or .gdb featureclass
+outfc = str(arcpy.GetParameterAsText(2))#'HFA_CO_test'#arcpy.GetParameterAsText(2) #name of table that will be imported to db
+scratch = arcpy.env.scratchFolder#r'C:\Users\JLoucks\Documents\JL\test1'
 arcpy.env.outputMFlag = "Disabled"
 arcpy.env.outputZFlag = "Disabled"
-infc = getspatial_ref(infc)
+indbconnection = str(getdb_connection(indbconnection))
+infc = str(getspatial_ref(infc,outfc))
 arcpy.FeatureClassToFeatureClass_conversion(infc,indbconnection,outfc)
